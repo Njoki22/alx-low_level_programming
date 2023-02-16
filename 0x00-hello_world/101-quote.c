ffe07995482a83887ec66bc45b122666ef0f4423
Njoki22 <njokimichelle22@gmail.com>
2023-02-16 08:13:50 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<unist.h>
+
 /**
  * main - Entry
  * Return: 1
  */
+int main(void)
 {
-	fput(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
-		return (1);
-}
+	write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n" ,);
+	return (1);
