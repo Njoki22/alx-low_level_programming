95460ced5a9388a6c50fa075887d714566199169
Njoki22 <njokimichelle22@gmail.com>
2023-02-16 05:07:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main- A program that runs a line using the printf function
- * Return:Always(0)
+ * main - Entry 
+ * Return: Always (0)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
