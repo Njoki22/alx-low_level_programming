8830b63ee7a93fbc5bd9637c9e957b2667fb0ddf
Njoki22 <njokimichelle22@gmail.com>
2023-02-16 03:22:49 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
  * main-Entry
- * Return:Always 0 (Success)
+ * Return:Always(0)
  */
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar but the outcome is a piece of art")
 		return(0);
 }
