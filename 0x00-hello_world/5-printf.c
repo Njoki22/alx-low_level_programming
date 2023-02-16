1a340f4ae32d10356ba89f29a54489baf859b997
Njoki22 <njokimichelle22@gmail.com>
2023-02-16 03:26:17 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return:Always(0)
  */
 {
-	printf("with proper grammar but the outcome is a piece of art")
-		return(0);
+	printf("\"with proper grammar, but the outcome is a piece of art,")
+		return (0);
 }
