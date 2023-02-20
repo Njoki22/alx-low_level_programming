74d3af507a2b848ebd479bfa71202f612a98d848
Njoki22 <njokimichelle22@gmail.com>
2023-02-20 00:04:46 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description:prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int n = 0;
+
+	for (x = 0; x < 10; x++)
+	{
+		n = 0;
+		for  (n = 0; n < 10; n++)
+		{
+			if (n != x && n < 10)
+			{
+				putchar('0' + n);
+				putchar('0' + x);
+
+				if (x + n != 18)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
