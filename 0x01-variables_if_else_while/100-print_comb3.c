f5a5f6df483f6f804e3fa78e167900231bfd2334
Njoki22 <njokimichelle22@gmail.com>
2023-02-20 02:01:27 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,8 +3,9 @@
 
 /**
  * main - Entry point
- * Description:prints all possible different combinations of two digits,
- * in ascending order, separated by a comma followed bya a space.
+ *
+ * Description - prints all possible different combinations of two digits in ascending order
+ *
  * Return: 0
  */
 int main(void)
@@ -12,26 +13,26 @@ int main(void)
 	int n;
 	int x = 0;
 
-	while (n < 10)
+	while (x < 10)
 	{
-		x = 0
-		while (x < 10)
+		n = 0;
+		while (n < 10)
 		{
-			if (x != &&x < n)
+			if (x != n && x < n)
 			{
 				putchar('0' + x);
 				putchar('0' + n);
 
-			if (x + n != 17)
-			{
-				putchar(',');
-				putchar(' ');
+				if (n + x != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			n++;
 		}
 		x++;
 	}
-	n++;
-
 	putchar('\n');
 	return (0);
 }
