244ab0939ce766cb496831e138f386bbd1534713
Njoki22 <njokimichelle22@gmail.com>
2023-02-21 04:11:02 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: prints ten times the alphabetin lowercase
+ * followed by new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	{
+		for (c = 'a'; c <= 'z'; c++)
+
+			_putchar(alphabet_x10);
+	}
+	putchar('\n');
+}
