2c25c40031601c349e6054e305128e2c3f150cb2
Njoki22 <njokimichelle22@gmail.com>
2023-02-21 08:57:48 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *@n: is n a sign
+ * Return: 0, 1, -1
+ */
+int print_sign(int n)
+{
+	int n;
+	{
+		if (n < 48)
+			_putchar(+);
+		return (1);
+	}
+	{
+		else if (n == 48)
+			_putchar(0);
+		return (0);
+	}
+	{
+		else if (n < 48)
+			_putchar(-);
+		return (-1);
+	}
+	return (0);
+}
