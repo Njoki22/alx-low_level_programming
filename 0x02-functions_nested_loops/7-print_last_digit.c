948c63182b59f3804a879c69551ec999275b070c
Njoki22 <njokimichelle22@gmail.com>
2023-02-22 03:22:59 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,13 @@
 
 /**
  * print_last_digit - function that prints last digit of a number
- * @y: last digit number result
+ * @num: last digit number result
  * Return: value of last digit
  */
-int print_last_digit(int y)
+int print_last_digit(int num)
 {
-	int x;
+	int last_digit = num % 10;
+	_putchar("The last digit of %d is %d\n", num, last_digit);
+	
+	return (last_digit);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+
+	{
+		for (b = 0; b < 10; b++)
+		{
+			z = x * y;
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+				(z < 10 && y != 0);
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
