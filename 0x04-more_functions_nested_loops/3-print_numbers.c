9c73b137e4060da8531923f5e000462c231b573f
Njoki22 <njokimichelle22@gmail.com>
2023-02-23 02:57:20 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 through 9 followed by new line
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	c = 0;
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+
+	_putchar('\n');
+	return (0);
+}
