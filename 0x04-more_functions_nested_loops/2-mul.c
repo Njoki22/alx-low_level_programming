c24129de9bfc147ec81756e4641780e565830246
Njoki22 <njokimichelle22@gmail.com>
2023-02-23 02:39:18 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 
 /**
- * mul - function that multiplies int a and int b 
+ * mul - function the multiplies int a and int b together
  * @a: int multiplied to b to get d
  * @b: int multiplied to a to get d
  * Return: d
  */
 int mul(int a, int b)
 {
+	int d;
+
+	d = a * b;
+	putchar('\n');
 	return (d);
 }
