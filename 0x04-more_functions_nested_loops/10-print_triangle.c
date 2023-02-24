ce9606406313dd311d84d5a755d41548c1c358cd
Njoki22 <njokimichelle22@gmail.com>
2023-02-24 03:29:22 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,18 +8,25 @@
 
 void print_triangle(int size)
 {
-	int m, n, o;
+int m, n, o;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
-	{
-		for (m = 0; m < size; m++)
-
-		for (n = size - m; n > 1; n--)
-			_putchar(32);
-		for (o = 0; o <= m; o++)
-			_putchar(35);
-	}
-	_putchar('\n');
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (m = 0; m < size; m++)
+{
+for (n = size - m; n > 1; n--)
+{
+_putchar(32);
+}
+for (o = 0; o <= m; o++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
 }
