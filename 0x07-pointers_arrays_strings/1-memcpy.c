ac0cae31e522d4fca8c29335e36c8db5f799aafd
Njoki22 <njokimichelle22@gmail.com>
2023-03-06 02:01:45 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @src: memory area to be copied from.
+ * @dest: memory area to put the copy.
+ * @n: size of memory.
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
