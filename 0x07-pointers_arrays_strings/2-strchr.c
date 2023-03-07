86dfd79a3f987b1b32b6910713719a3382bcfa01
Njoki22 <njokimichelle22@gmail.com>
2023-03-06 20:59:50 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i < *s; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
+		if (s[i] == c)
+			return (s + i);
 	}
-	if (c == '\0')
-		return (s);
-
 	return (NULL);
 }
