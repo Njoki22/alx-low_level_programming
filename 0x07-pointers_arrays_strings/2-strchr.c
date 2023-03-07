ad31367edad4ce8ae063b24c9ac481fea663a936
Njoki22 <njokimichelle22@gmail.com>
2023-03-06 21:02:04 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,8 +2,8 @@
 
 /**
  * _strchr - function that locates a character in a string.
- * @s: The string to check.
- * @c: character to be identified.
+ * @s: input
+ * @c: input
  * Return: pointer to the first occurrence of the character c in the string s
  * or NULL if the character is not found
  */
