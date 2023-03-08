718c16d63eef4b8666f4ecd7851109edda6bc24c
Njoki22 <njokimichelle22@gmail.com>
2023-03-08 08:59:21 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,25 +16,3 @@ int _sqrt_recursion(int n)
 	return (sqrt_fun(n, sq));
 }
 
-/**
- * sqrt_fun - helper function to solve _sqrt_recursion
- * @sq: number to determine if square root
- * @n:  int number
- * Return: square root if natural square root, or -1 if none found
- */
-
-int sqrt_fun(int n, int sq)
-{
-	if (n == sq * sq)
-	{
-		return (sq);
-	}
-	else if (sq < n)
-	{
-		return (sqrt_fun(n, ++sq));
-	}
-	else
-	{
-		return (-1);
-	}
-}
