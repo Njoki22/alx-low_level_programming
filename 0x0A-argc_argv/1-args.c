54a250fea48f7dc89699f4a88dadc4dbb2673579
Njoki22 <njokimichelle22@gmail.com>
2023-03-10 08:31:50 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number,
+ * followed by new line
+ * @argc: number of commandline arguments
+ * @argv: array of strings
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	int num_args = argc - 1;
+	printf("%d\n", num_args);
+	return (0);
+}
