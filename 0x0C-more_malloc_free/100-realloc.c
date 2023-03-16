911f92b07a1f70c0bb5ea8ac8458d25f06b6cbe7
Njoki22 <njokimichelle22@gmail.com>
2023-03-16 08:02:29 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -17,19 +16,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0)
 	{
-		return (NULL);
-	}
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
 	{
-		return (NULL);
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
 	}
+	else
+	{
+		if (new_size <= old_size)
+		{
+			return (ptr);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
 
-	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
-	free(ptr);
+		memcpy(new_ptr, ptr, old_size);
+		free(ptr);
+	}
 	return (new_ptr);
 }
+
