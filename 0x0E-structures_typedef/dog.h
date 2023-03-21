108cc8716408fb96218fb3fb97a24546de810b04
Njoki22 <njokimichelle22@gmail.com>
2023-03-21 02:50:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,16 +3,17 @@
 
 /**
  * struct dog - Dog attributes
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: Attributes of the dog
+ * Description: The attributes of a dog.
  */
+
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 typedef struct dog dog_t;
