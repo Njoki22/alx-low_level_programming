93fc4af3cd4b78ce1cf2913233cfb51cc7a10b85
Njoki22 <njokimichelle22@gmail.com>
2023-03-22 05:02:00 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * get_op_func - selects the correct function to performthe operationasked by user
+ * @s: operator passed as argument
+ *
+ * Return: pointer to the function that corresponds to the operator
+ * given as parameter. NULL if the operator is not supported
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	opt_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if ((*ops[i].op == *s && *(s + 1) == '\0'))
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of division of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of division of a and b
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * op_mod - return the remainder of a / b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a / b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
