b33cc161ff2eba125b65baa1ecbafe9c6fe5cb97
Njoki22 <njokimichelle22@gmail.com>
2023-03-24 04:25:16 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,38 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
+ * main - return result of desired calculation from command line options
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: EXIT_FAILURE if unsuccessful or EXIT_SUCCESS if success.
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int num1, num2, calc;
+	int (*op_func)(int, int);
+	char *operator;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
+	operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	op_func = get_op_func(operator);
+	if (op_func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+	calc = op_func(num1, num2);
+	printf("%d\n", calc);
+	exit(EXIT_SUCCESS);
 }
