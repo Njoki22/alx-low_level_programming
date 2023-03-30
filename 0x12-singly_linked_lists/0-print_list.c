a103827e2135ec0d5c90460f0a48936bb922fdd6
Njoki22 <njokimichelle22@gmail.com>
2023-03-30 05:45:53 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if ((*h).str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
-		h = h->next;
+		h = (*h).next;
 	}
-
 	return (count);
 }
