cc6246bedb347f8adf34bac65a6bc39c59fbb1bf
Njoki22 <njokimichelle22@gmail.com>
2023-03-30 05:41:25 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include<stdio.h>
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node, *next_node;
+
+	current_node = head;
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node->str);
+		free(current_node);
+		current_node = next_node;
+	}
+}
