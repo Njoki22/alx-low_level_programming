7b4f2dbd13bed3c4217871aeafe8b2990ac31668
Njoki22 <njokimichelle22@gmail.com>
2023-04-04 05:11:48 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to the beginning of a list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
