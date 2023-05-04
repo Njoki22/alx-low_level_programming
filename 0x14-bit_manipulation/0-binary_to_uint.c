28ebfcf6a1f2c5f645f2d86a8d4de2e438dbbc0b
Njoki22 <njokimichelle22@gmail.com>
2023-05-04 09:25:51 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	int d = 0;
+	int len = strlen(b);
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	while (b[d] == '0' || b[d] == '1')
+	for (d = 0; d < len; d++)
 	{
-		result = (result << 1) | (b[d] - '0');
-		d++;
+		if (b[d] == '0' || b[d] == '1')
+			result = (result << 1) | (b[d] - '0');
+		else
+			return (0);
 	}
-
 	return (result);
 }
